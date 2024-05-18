251be43cb64068f3c8d93a2678614f7772e93751
Giacomo Milani <giacomo83m@gmail.com>
2024-05-18 15:40:40 +0200

    code cleanup and improved tool docstring

--- duckcatgo.py
+++ duckcatgo.py
@@ -2,43 +2,82 @@ from cat.mad_hatter.decorators import tool
 from cat.looking_glass.stray_cat import StrayCat
 from cat.log import log
 from duckduckgo_search import DDGS
+from json import loads, JSONDecodeError
+from typing import List, Dict, Any, Tuple
+from .settings import DuckGoSettings
 from .scraper import webscrape
-from json import loads as json_parse
-from typing import List, Dict
 
 
-@tool(examples=["Can you search for the next music events in Milan?", "Can you find me a good movie on netflix?"])
+def json_parse(input_str: str) -> Tuple[Dict[str, Any], bool]:
+    try:
+        result_dict = loads(input_str)
+        return result_dict, True
+    except JSONDecodeError:
+        return {}, False
+
+
+@tool(examples=["Can you search for the next music events in Milan?", "Can you find me a good movie on Netflix?"])
 def duckcat_search(query: str, cat: StrayCat) -> str:
-    """Search for answers. The input is a query"""
+    """
+    Useful for searching online information that can change over
+    time, like upcoming events, prices, values, ages, rates,
+    weather, addresses, road conditions, breaking news, and
+    means of transportation. For example, a user may inquire,
+    "What is the current value of a bitcon." The "query" argument is the
+    input text submitted to a search engine.
+    """
+    settings: DuckGoSettings = cat.mad_hatter.get_plugin().load_settings()
+    pages: List[str] = []
+    language_code = settings.get("lang_code", "en")
+    max_bytes = settings.get("max_bytes_scraped", 8192)
+
     with DDGS() as ddg:
-        settings = cat.mad_hatter.get_plugin().load_settings()
-        pages: List[str] = []
-        for result in ddg.text(query, region=settings.get("lang_code", "en"), max_results=3):
-            href: str = result.get('href', '')
+        for result in ddg.text(query, region=language_code, max_results=3):
+            href: str = result.get("href", "")
             log.info(f"webscraping {href}")
             body = webscrape(href, cat)
-            llm_text_eval = cat.llm(f"this is the content i scraped from a webpage, please clean up all the page artifacts and return me onlythe relevant and meaningful content of this page. I need also to know if this content is enough to answer to the question: {query}. The output MUST be only a json object with two keys, relevant_text(str) and is_enough(bool).\ncontent:\n {body}")
-            log.info(f"eval result: {llm_text_eval}")
-            llm_eval = json_parse(llm_text_eval)
-            pages.append(llm_eval.get("relevant_text", ''))
-            if llm_eval.get("is_enough", False) == True:
+            llm_request = (
+                f"This is the content I scraped from a webpage, please clean up all the page artifacts "
+                f"and return only the relevant and meaningful content of this page. I also need to know if "
+                f"this content is sufficient to answer the question: {query}. The output must be only a JSON object "
+                f"with two keys: 'relevant_text' (str) and 'is_enough' (bool).\ncontent:\n{body}"
+            )
+            llm_text_eval = cat.llm(llm_request)
+            eval_result, success = json_parse(llm_text_eval)
+            if not success:
+                log.info(f"error parsing llm result: {llm_text_eval}")
+                continue
+            if "relevant_text" not in eval_result or "is_enough" not in eval_result:
+                log.info(f"eval_results is missing relevant_text or is_enough keys: {eval_result}")
+                continue
+            pages.append(eval_result.get("relevant_text", ''))
+            if eval_result.get("is_enough", False):
                 break
-        text: str = '\n'.join(pages)
-        text = text[:settings.get("max_bytes_scraped", 8192)]
-        return f"Please summarize the most important information about {query} from the text found online:\n{text}"
+
+        combined_text = "\n".join(pages)[:max_bytes]
+        return (
+            f"Please summarize the most important information about {query} from the text found online: "
+            f"\n{combined_text}"
+        )
 
 
 @tool(examples=["BBC website"])
 def duckcat_website(entity: str, cat: StrayCat) -> str:
-    """what is the website of that entity?. The input is an entity"""
+    """
+    Retrieves the website associated with a specified entity.
+    The 'entity' parameter refers to the name or identifier of the
+    entity for which the website needs to be found.
+    """
+    settings = cat.mad_hatter.get_plugin().load_settings()
+    language_code = settings.get("lang_code", "en")
+    max_bytes = settings.get("max_bytes_scraped", 8192)
+
     with DDGS() as ddg:
-        settings = cat.mad_hatter.get_plugin().load_settings()
         sections: List[str] = []
-        for result in ddg.text(entity, region=settings.get("lang_code", "en"), max_results=3):
+        for result in ddg.text(entity, region=language_code, max_results=5):
             title = result.get('title', '')
             href = result.get('href', '')
             body = result.get('body', '')
             sections.append(f"{title}\n{href}\n{body}\n\n")
-        text: str = '\n'.join(sections)
-        text = text[:settings.get("max_bytes_scraped", 8192)]
-        return f"Websites related to {entity}: \n{text}"
+        combined_text = "\n".join(sections)[:max_bytes]
+        return f"Websites related to {entity}: \n{combined_text}"
